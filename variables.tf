c68387e7613fb09242899122fc870beb3878807e
Anton Patsev <patsev.anton@gmail.com>
2024-07-10 17:20:26 +0600

    Добавлены комментарии в код

--- variables.tf
+++ variables.tf
@@ -2,97 +2,97 @@
 # Координаты облака Yandex
 #
 variable "folder_id" {
-description = "Идентификатор папки"
-type        = string
-default     = null
+  description = "Идентификатор папки"
+  type        = string
+  default     = null
 }
 
 variable "region_id" {
-description = "Идентификатор зоны доступности, где находится ALB"
-type        = string
-default     = null
+  description = "Идентификатор зоны доступности, где находится ALB"
+  type        = string
+  default     = null
 }
 
 #
 # Именование
 #
 variable "name" {
-description = "Имя ALB"
-type        = string
+  description = "Имя ALB"
+  type        = string
 }
 
 variable "description" {
-description = "Описание ALB"
-type        = string
-default     = ""
+  description = "Описание ALB"
+  type        = string
+  default     = ""
 }
 
 variable "labels" {
-description = "Набор меток"
-type        = map(string)
-default     = {}
+  description = "Набор меток"
+  type        = map(string)
+  default     = {}
 }
 
 #
 # Сеть
 #
 variable "network_id" {
-description = "Идентификатор сети, в которой находится ALB"
-type        = string
+  description = "Идентификатор сети, в которой находится ALB"
+  type        = string
 }
 
 variable "security_group_ids" {
-description = "Список идентификаторов групп безопасности, присоединенных к ALB"
-type        = list(string)
-default     = []
+  description = "Список идентификаторов групп безопасности, присоединенных к ALB"
+  type        = list(string)
+  default     = []
 }
 
 variable "subnets" {
-description = "Список подсетей"
-default     = {}
+  description = "Список подсетей"
+  default     = {}
 }
 
 variable "create_pip" {
-description = "Если true, будет создан публичный IP"
-type        = bool
-default     = true
+  description = "Если true, будет создан публичный IP"
+  type        = bool
+  default     = true
 }
 
 variable "pip_zone_id" {
-description = "Зона для публичного IP"
-type        = string
-default     = "ru-central1-a"
+  description = "Зона для публичного IP"
+  type        = string
+  default     = "ru-central1-a"
 }
 
 #
 # Логирование
 #
 variable "enable_logs" {
-description = "Установите true, чтобы отключить Cloud Logging для балансировщика"
-type        = bool
-default     = true
+  description = "Установите true, чтобы отключить Cloud Logging для балансировщика"
+  type        = bool
+  default     = true
 }
 
 variable "log_group_id" {
-description = "Идентификатор группы Cloud Logging для отправки логов. Оставьте пустым, чтобы использовать группу логов по умолчанию для папки балансировщика"
-type        = string
-default     = ""
+  description = "Идентификатор группы Cloud Logging для отправки логов. Оставьте пустым, чтобы использовать группу логов по умолчанию для папки балансировщика"
+  type        = string
+  default     = ""
 }
 
 variable "discard_rules" {
-description = "Список правил игнорирования логов"
-type = object({
-http_codes          = optional(list(string), [])
-http_code_intervals = optional(number)
-grpc_codes          = optional(list(string), [])
-})
-default = null
+  description = "Список правил игнорирования логов"
+  type = object({
+    http_codes          = optional(list(string), [])
+    http_code_intervals = optional(number)
+    grpc_codes          = optional(list(string), [])
+  })
+  default = null
 }
 
 #
 # Конфигурация балансировщика нагрузки
 #
 variable "listeners" {
-description = "Слушатели балансировщика нагрузки приложений"
-default     = {}
+  description = "Слушатели балансировщика нагрузки приложений"
+  default     = {}
 }
