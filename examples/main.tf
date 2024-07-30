3d02f9d7f0a7542398f4d12deb7b8f62e46efbe8
Anton Patsev <patsev.anton@gmail.com>
2024-07-30 20:44:25 +0600

    Добавил в пример module self_managed

--- examples/main.tf
+++ examples/main.tf
@@ -54,6 +54,89 @@ module "self_managed" {
   }
 }
 
+module "iam_accounts" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-iam.git//modules/iam-account?ref=v1.0.0"
+
+  name = "iam-yandex-compute-instance-group"
+  folder_roles = [
+    "editor"
+  ]
+  cloud_roles              = []
+  enable_static_access_key = false
+  enable_api_key           = false
+  enable_account_key       = false
+
+}
+
+module "instance_group" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-instance-group.git"
+
+  zones = ["ru-central1-a"]
+
+  name = "example-instance-group"
+
+  network_id = module.network.vpc_id
+  subnet_ids = [module.network.private_subnets_ids[0]]
+  enable_nat = true
+
+  scale = {
+    fixed = {
+      size = 1
+    }
+  }
+
+  max_checking_health_duration = 10
+
+  health_check = {
+    enabled             = true
+    interval            = 15
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    tcp_options = {
+      port = 22
+    }
+  }
+
+  platform_id   = "standard-v3"
+  cores         = 2
+  memory        = 4
+  core_fraction = 100
+
+  image_family = "ubuntu-2004-lts"
+
+  enable_alb_integration = true
+
+  hostname           = "my-instance"
+  service_account_id = module.iam_accounts.id
+  ssh_user           = "ubuntu"
+  generate_ssh_key   = false
+  ssh_pubkey         = "~/.ssh/id_rsa.pub"
+
+  user_data = <<-EOF
+        #cloud-config
+        package_upgrade: true
+        packages:
+          - nginx
+        runcmd:
+          - [systemctl, start, nginx]
+          - [systemctl, enable, nginx]
+        EOF
+
+  boot_disk = {
+    mode        = "READ_WRITE"
+    device_name = "boot"
+  }
+
+  boot_disk_initialize_params = {
+    size = 30
+    type = "network-ssd"
+  }
+
+  depends_on = [module.iam_accounts]
+}
+
+
 
 module "alb" {
   source = "../"
@@ -121,7 +204,7 @@ module "alb" {
         weight = 100
         http2  = true
         target_group_ids = [
-          "MyExampleTargetGroupId"
+          module.instance_group.target_group_id
         ]
         health_check = {
           timeout                 = "30s"
@@ -156,7 +239,7 @@ module "alb" {
         weight = 100
         http2  = true
         target_group_ids = [
-          "MyExampleTargetGroupId"
+          module.instance_group.target_group_id
         ]
         health_check = {
           timeout                 = "30s"
--- examples/outputs.tf
+++ examples/outputs.tf
@@ -6,3 +6,13 @@ output "domain_com_certificate" {
 output "id" {
   value = module.self_managed.self_managed_certificates["domain-com"].id
 }
+
+output "instance_group_id" {
+  description = "Compute instance group ID"
+  value       = module.instance_group.instance_group_id
+}
+
+output "target_group_id" {
+  description = "Target group ID"
+  value       = module.instance_group.target_group_id
+}
